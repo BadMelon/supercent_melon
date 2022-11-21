5565e8d5e4a43e06c86faf3745e44e21cf869b6c
BadMelon <93146798+BadMelon@users.noreply.github.com>
2022-11-21 17:48:07 +0900

    listAndLinkedList

---
+++ list/Assets/linkedList.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linkedList : StudyBase
+{
+	protected override void OnLog()
+	{
+		var lList = new LinkedList<string>();
+
+		lList.AddFirst("My name is");
+		lList.AddLast("AlphaGo");
+		lList.AddLast("Hi");
+		// My name is, AlphaGo, Hi
+		lList.LogValues();
+
+		lList.Remove("Hi");
+		lList.AddFirst("Hello");
+		// Hello, My name is, AlphaGo
+		lList.LogValues();
+
+		lList.RemoveFirst();
+		lList.AddLast("I'm glad to meet you");
+		// My name is, AlphaGo, I'm glad to meet you
+		lList.LogValues();
+	}
+
+
+	public class LinkedListNode<T>
+	{
+		public LinkedListNode<T> Head;
+		public LinkedListNode<T> Tail;
+		public T data;
+	}
+
+	public sealed class LinkedList<T> : IEnumerable<T>
+	{
+		// 양방향 링크로 구현하세요
+		public int Count { private set; get; } = 0;
+		LinkedListNode<T> head = null;
+		LinkedListNode<T> tail = null;
+
+		LinkedList<T> linkedlist;
+		public LinkedListNode<T> First
+		{
+            set
+            {
+				head.Head = null;
+				head.Tail = Last.Head;
+            }
+			get
+			{
+				return head;
+			}
+		}
+		public LinkedListNode<T> Last
+		{
+            set
+            {
+				tail.Head = First.Tail;
+				tail.Tail = null;
+            }
+			get
+			{
+				return tail;
+			}
+		}
+
+
+		public bool Contains(T value)
+		{
+			foreach(var v in linkedlist)
+            {
+				if(EqualityComparer<T>.Default.Equals(value, v))
+                {
+					return true;
+                }
+            }
+			return false;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+		public IEnumerator<T> GetEnumerator()
+		{
+			var list = new LinkedList<T>();
+			foreach(var v in list)
+            {
+				yield return v;
+            }
+		}
+
+
+		public LinkedListNode<T> AddFirst(T value)
+		{
+			
+			head = First;
+
+			return head;
+
+		}
+		public LinkedListNode<T> AddLast(T value)
+		{
+			
+			head = Last;
+
+			return head;
+		}
+
+		public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
+		{
+			return head;
+		}
+		public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value)
+		{
+			return head;
+		}
+
+		public bool Remove(T value)
+		{
+			return true;
+		}
+		public void Remove(LinkedListNode<T> node)
+		{
+			if (node == null)
+				throw new Exception($"{nameof(node)}가 null 입니다.");
+			
+		}
+
+		public void RemoveFirst()
+		{
+			
+		}
+		public void RemoveLast()
+		{
+			
+		}
+
+		public void Clear()
+		{
+			
+		}
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ list/Assets/list.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace melon
+{
+
+    public class list : StudyBase
+    {
+		protected override void OnLog()
+		{
+			var aList = new List<int>();
+
+			aList.Add(2);
+			// 2
+			aList.LogValues();
+
+			aList.Insert(0, 1);
+			// 1, 2
+			aList.LogValues();
+
+			aList.Add(4);
+			aList.Insert(aList.Count - 1, 3);
+			// 1, 2, 3, 4
+			aList.LogValues();
+
+			aList.Remove(2);
+			aList.RemoveAt(0);
+			// 4
+			Log(aList[aList.Count - 1]);
+		}
+		public sealed class List<T> : IEnumerable<T>
+		{
+			
+			public int Count { private set; get; } = 0;
+			T[] array = new T[4];
+			
+			public T this[int index]
+			{
+				
+				set
+				{
+					array[index] = value;
+				}
+				get
+				{
+					return array[index];
+				}
+			}
+			
+
+			public bool Contains(T value)
+			{
+				for(int i = 0; i < array.Length; i++)
+                {
+					if (EqualityComparer<T>.Default.Equals(array[i], value))
+						return true;
+                }
+				return false;
+			}
+
+			IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+			public IEnumerator<T> GetEnumerator()
+			{
+				for(int i = 0; i< Count; i++)
+                {
+					yield return array[i];
+                }
+			}
+
+
+			public void Add(T value)
+			{
+				if(Count == array.Length)
+                {
+					//array의 길이 2배증가
+					System.Array.Resize(ref array, array.Length * 2);
+                }
+				array[Count++] = value;
+			}
+			public void Insert(int index, T value)
+			{
+				if (Count == array.Length)
+				{
+					//array의 길이 2배증가
+					System.Array.Resize(ref array, array.Length * 2);
+				}
+				for (int i = Count; index < i ; i--)
+                {
+					array[i] = array[i - 1];
+                }
+				array[index] = value;
+				Count++;
+
+			}
+
+			public bool Remove(T value)
+			{
+				for(int i = 0; i< Count; i++)
+                {
+					if(EqualityComparer<T>.Default.Equals(array[i], value))
+                    {
+						for(int j = i; j<Count - 1; j++)
+                        {
+							array[j] = array[j + 1];
+                        }
+                    }
+                }
+				Count--;
+				return false;
+			}
+			public void RemoveAt(int index)
+			{
+				for(int i = index; i< Count; i++)
+                {
+					array[i] = array[i + 1];
+                }
+				Count--;
+			}
+
+			public void Clear()
+			{
+				
+			}
+	}
+
+		
+		// Start is called before the first frame update
+		void Start()
+        {
+		
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
